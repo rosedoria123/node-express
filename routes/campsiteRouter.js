84cb4e014c9a63610ef05e0f1f46949ec3d551a9
Rosa Doria <rosedoria123@gmail.com>
2023-08-13 20:52:42 -0500

    Task 1: workshop 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const campsiteRouter = express.Router();
 
-campsiteRouter.route('/')
+campsiteRouter.route('/:campsiteId')
 .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
 .get((req, res) => {
-    res.end('Will send all the campsites to you');
+    res.end(`Will send details of campsite: ${req.params.campsiteId}`)
 })
 .post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+    res.statusCode = 403;
+    res.end('Post not supported')
 })
 .put((req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
+    res.end(`Will update the campsite:${req.body.name} and description: ${req.body.description}`)
 })
 .delete((req, res) => {
-    res.end('Deleting all campsites');
+    res.end(`Deleting campsite: ${req.params.campsiteId}`)
 });
 
 module.exports = campsiteRouter;
